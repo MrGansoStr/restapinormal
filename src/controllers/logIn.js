dcda9f62e94fe4e352eea19b0d7b7887efa36e08
MrGansoStr <ccounttnro1@gmail.com>
2022-11-04 19:05:49 -0500

    Added login

--- src/controllers/logIn.js
+++ src/controllers/logIn.js
@@ -1,11 +1,29 @@
 const connectDB = require('../db.js');
 
 const logIn = async (req, res) => {
-  const connection = await connectDB();
-  let user = {...req.body};
-  let [ result ] = await connection.query(`SELECT * FROM users WHERE username='${user.username}'`);
-  console.log(result)
-  res.status(200).json(result)
+  try {
+    const connection = await connectDB();
+    let user = {...req.body};
+    let [ result ] = await connection.query(`SELECT username FROM users WHERE username='${user.username}'`);
+    //console.log(result)
+    if(result.length == 0) {
+      return res.status(401).json("Invalid Username or Password");
+    }
+    else{
+      let [ vresult ] = await connection.query(`SELECT username, password, email FROM users WHERE username ='${user.username}' AND password = '${user.password}'`);
+      if(vresult.length == 0) {
+        return res.status(401).json("Invalid Username or Password");
+      }
+      else {
+        return res.status(200).json(vresult);
+      }
+      return;
+    }
+    
+  } catch (error) {
+    res.status(400);
+    return console.log(error).json();
+  }
 };
 
 module.exports = logIn;
