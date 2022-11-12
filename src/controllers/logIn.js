3fe11515a83612d1f5471f5103f4f31d8ff0713d
MrGansoStr <ccounttnro1@gmail.com>
2022-11-12 18:03:24 -0500

    token and encrypt password

--- src/controllers/logIn.js
+++ src/controllers/logIn.js
@@ -1,21 +1,37 @@
 const connectDB = require('../db.js');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const logIn = async (req, res) => {
+  
   try {
-    const connection = await connectDB();
     let user = {...req.body};
+    if(!(user.username && user.password)) {
+      res.status(400).send("All inputs is required");
+    }
+    const connection = await connectDB();
+    
     let [ result ] = await connection.query(`SELECT username FROM users WHERE username='${user.username}'`);
     //console.log(result)
     if(result.length == 0) {
       return res.status(401).json("Invalid Username or Password");
     }
     else{
-      let [ vresult ] = await connection.query(`SELECT username, password, email FROM users WHERE username ='${user.username}' AND password = '${user.password}'`);
+      let [ vresult ] = await connection.query(`SELECT username, password, email FROM users WHERE username ='${user.username}'`);
       if(vresult.length == 0) {
         return res.status(401).json("Invalid Username or Password");
       }
       else {
-        return res.status(200).json(vresult);
+        await bcrypt.compare(user.password, vresult[0].password, (err, ok) => {
+          if(!err && ok ){
+            const token = jwt.sign({name: user.username},"secretkeyxd", {
+              expiresIn: "10m",
+            });
+            res.status(200).send({"message": "Login Successfully", "accessToken": token, "expiresIn": "10 min"});
+          }
+          else {
+            res.status(400).send({"message": "Something Wrong"});
+          }
+        });
       }
       return;
     }
--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,19 +1,22 @@
 const connectDB = require('../db.js');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const Register = async (req, res) => {
-  let user = {...req.body}
+  let user = {...req.body};
+  
   try {
     const connection = await connectDB();
+    const passhashed = await bcrypt.hash(user.password, 10);
     let [ result ] = await connection.query(`SELECT username FROM users WHERE username = '${user.username}'`);
     if(result.length > 0) {
       return res.status(422).send({message: "Usuario ya en uso"});
     }
     else {
       let [ newresult ] = await connection.query(`INSERT INTO users (username, password, email) 
-      VALUES ('${user.username}', '${user.password}', '${user.email}')`);
+      VALUES ('${user.username}', '${passhashed}', '${user.email}')`);
       //console.log(newresult);
       //res.json(newresult);
-      return res.status(200).send({message: "Register Successfully"});
+      return res.status(200).send({message: `Register Successfully`});
     }
   } catch (error) {
     console.log(error);
